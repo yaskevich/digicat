0a986d049a9bee885f7cfeddacc4c56550158ed4
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-02-02 17:12:44 +0300

    update object sent via api/projects route

--- server/index.js
+++ server/index.js
@@ -28,31 +28,46 @@ const info = {
   material: {}, era: {}, subject: {}, language: {},
 };
 
+const index = {};
+
 /* eslint-disable-next-line no-restricted-syntax */
 for (const item of data) {
   const id = item._attributes['xml:id'];
+  const obj = {};
   /* eslint-disable no-restricted-syntax, no-unused-expressions */
   for (const unit of item.text.body.desc) {
     const { type } = unit._attributes;
     if (['language', 'era', 'subject'].includes(type)) {
       const vals = unit._attributes?.ana.split(' ');
+      obj[type] = vals;
       for (const val of vals) {
         info[type]?.[val] ? info[type][val].push(id) : info[type][val] = [id];
       }
     } else if (['material',].includes(type)) {
       const val = unit._attributes?.ana;
       info[type]?.[val] ? info[type][val].push(id) : info[type][val] = [id];
+      obj[type] = val;
     }
   }
+  const title = item.text.body.bibl.title._text.toString().replace(/[\t\n\s]+/g, ' ').replaceAll(' - ', ' – ').trim();
+  if (item.text.body.bibl?.ref?._text) {
+    const url = item.text.body.bibl.ref._text.toString();
+    index.url = url;
+  }
+  obj.title = title;
+  obj.id = id;
+
+  index[id] = obj;
 }
 // console.log(info);
 
-app.get('/api/size', async (req, res) => {
-  res.json({ size: data.length });
-});
+// app.get('/api/size', async (req, res) => {
+//   res.json({ size: data.length });
+// });
 
 app.get('/api/projects', async (req, res) => {
-  res.json(data.map((x) => ({ id: x._attributes['xml:id'], title: x.text.body.bibl.title._text.toString().replace(/[\t\n\s]+/g, ' ').replaceAll(' - ', ' – ').trim() })));
+  // res.json(data.map((x) => ({ id: x._attributes['xml:id'], title: x.text.body.bibl.title._text.toString().replace(/[\t\n\s]+/g, ' ').replaceAll(' - ', ' – ').trim() })));
+  res.json(index);
 });
 
 app.get('/api/data', async (req, res) => {
