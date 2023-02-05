7c9b73175d862f25cadb45a7fb86384f199bdcc5
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-02-05 20:25:38 +0300

    enable custom font

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,6 +10,14 @@ import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 
 import PrimeVue from 'primevue/config';
+
+import "@fontsource/roboto-condensed" // Defaults to weight 400 with normal variant.
+import "@fontsource/roboto-condensed/400-italic.css" // Italic variant.
+import "@fontsource/roboto-condensed/700.css" // Bold variant.
+import "@fontsource/roboto-condensed/700-italic.css" // Bold italic variant.
+// import "@fontsource/roboto-condensed/900.css" // Black variant.
+
+
 const app = createApp(App);
 
 app.use(PrimeVue).use(router).use(JsonViewer).mount('#app');
