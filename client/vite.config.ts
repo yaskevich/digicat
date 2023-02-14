92d478e1e5a90eda91d6e7466a188e048f30dbd4
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-02-14 12:02:28 +0300

    set HTML title via Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,25 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Checker from 'vite-plugin-checker'
-import { VitePWA } from 'vite-plugin-pwa'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Checker from 'vite-plugin-checker';
+import { VitePWA } from 'vite-plugin-pwa';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import htmlPlugin from 'vite-plugin-html-config';
+import fs from 'fs';
+import path from 'path';
+
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'server', 'data', 'catalog.json'), 'utf8'));
+
+const htmlPluginOpt = {
+  title: `CDSE ${data.gitlab.last_activity_at.slice(0, 7)} [${Object.keys(data.projects).length}]`,
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate', devOptions: {
-        enabled: true
-      }
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
     }),
     Checker({ typescript: true }),
     AutoImport({
-      imports: [
-        'vue',
-      ],
+      imports: ['vue'],
     }),
+    htmlPlugin(htmlPluginOpt),
   ],
   server: {
     port: 5000,
@@ -29,7 +38,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         ws: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
